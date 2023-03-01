3b8c661bc6689f254fd4031caf5dfa47186489a9
MadeleineKRoberts <mkroberts@uchicago.edu>
2023-03-01 11:40:21 -0600

    doc strings

--- scrapers/scrape_lawndale.py
+++ scrapers/scrape_lawndale.py
@@ -1,3 +1,23 @@
+"""
+Project: CAPP 122 DataBased Project
+File name: scrape_lawndale.py
+
+Scrapes data from lawndale news pages pages.
+
+Methods:
+    * scrape_ln - scrapes data for mayoral candidates.
+    * scrape_all_pages - scrapes data for passed candidate tokens.
+    * get_first_search_page - finds first page of search results for passed candidate tokens.
+    * get_article_urls - finds all park URLs on page.
+    * get_next_page - finds URL to next page of article lists.
+    * scrape_article - scrapes article page.
+    * date_convert - converts string to a datetime object.
+    * make_request - make a request to 'url' and return the raw response.
+
+@Author: Madeleine Roberts
+@Date: Feb 26, 2023
+"""
+
 import sys
 import json
 import lxml.html
@@ -12,6 +32,13 @@ sys.path.append(parent)
 from utilities.data_retrieval import search_strings
 
 def scrape_ln():
+    """
+    Scrapes all articles from lawndale news that are associated with the candidates 
+    and candidate tokens in the database.
+
+    Returns: 
+        * A json file that contains all scraped articles for all candidates.
+    """
     # Retrieve all candidate information from database
     cand_data = search_strings('news_ln')
     cand_data = cand_data.to_dict('index')
@@ -21,6 +48,7 @@ def scrape_ln():
     # Run scraper for each unique token and output to json file
     for _, val in cand_data.items():
         announcement_date = date_convert(val['announcement_date'], 1)
+
         article_list_for_token = scrape_all_pages(val['candidate_id'], val['name_tokens'], announcement_date)
         json_list += article_list_for_token
         
@@ -30,7 +58,18 @@ def scrape_ln():
         json.dump(json_list, f, indent=1)
 
 def scrape_all_pages(candid, name_tokens, announcement_date):
-    # Assumming passed in date is a date object
+    """
+    This function takes a candidate id, the name tokens to search for, and their
+    announcement date to scrape all articles associated with these tokens.
+
+    Parameters:
+        * candidate_id: candidate id in database
+        * name_tokens:  specified search string
+        * announcement_date: date of candidate announcement
+
+    Returns:
+        A list of scraped articles
+    """
 
     # Retreive correct search url
     current_url = get_first_search_page(name_tokens) 
@@ -54,6 +93,16 @@ def scrape_all_pages(candid, name_tokens, announcement_date):
     return list_of_scraped_pages
     
 def get_first_search_page(full_name):
+    """
+    Retrieves the first page of search results for a specific name token.
+
+    Parameters:
+        * full_name (string): name tokens to include in the search
+
+    Returns:
+        URL to the first search page of the given tokens
+    """
+    
     split_name = full_name.split()
     search_name = "%22" + "+".join(split_name) + "%22+mayor"
     url = f"http://www.lawndalenews.com/?s={search_name}&x=0&y=0"
@@ -107,7 +156,17 @@ def get_article_urls(url, announcement_date):
     return urls
 
 def get_next_page(url):
-    # TODO maybe pass the root node
+    """
+    This function takes a URL to a page of newpaper articles and returns a
+    URL to the next page of articles if one exists.
+
+    Parameters:
+        * url (string): url to page of newpaper articles
+    
+    Returns:
+        A string that contains the next page of newpaper articles. If no next page exists, this function returns None.
+    """
+    
     page = make_request(url)
     root = lxml.html.fromstring(page.text)
 
@@ -130,10 +189,33 @@ def get_next_page(url):
     else:
         # single page of results
         return None
-        # Page has changed
-        #raise Exception ("Page has changed: check next link buttons/structure")
+
 
 def scrape_article(url, cand_id, name_tokens, announcement_date):
+     """
+    This function takes a URL to a Lawndale newspaper article page and its repesective 
+    database tokens and returns a dictonary with database tokens (candidate_id,
+    name_tokens, announcement_date, and newspaper_id), url to article, article title,
+    article text, and article date.
+
+    Parameters:
+        * url (string):  a URL to a newspaper aticle page
+        * cand_id (string): candidate id in database
+        * name_tokens (string): specified search string
+        * announcement_date (datetime object): date of candidate announcemnt
+
+    Returns:
+        A dictionary with the following keys:
+            * candidate_id: candidate id in database
+            * name_tokens:  specified search string
+            * announcement_date: date of candidate announcement
+            * newspaper_id: newspaper id in database
+            * url:          the URL of the article page
+            * title:        the title of article
+            * text:         the text content of the article
+            * date:         the description of the park
+    """
+    
     page = make_request(url)
     root = lxml.html.fromstring(page.text)
 
@@ -155,7 +237,6 @@ def scrape_article(url, cand_id, name_tokens, announcement_date):
     # Note: this includes the author and editor; this should be removed in the cleaning 
     full_text = article[0].cssselect("div.entry-content")[0].text_content()
 
-    # pass in the missing args? 
     full_article = {
         'candidate_id' : cand_id,
         'name_tokens' : name_tokens,
@@ -170,6 +251,16 @@ def scrape_article(url, cand_id, name_tokens, announcement_date):
     return full_article
 
 def date_convert(date, flag):
+    """
+    Converts string to a datetime object
+
+    Parameters:
+        * date (string): the date to be converted
+        * flag (int): indicates the structure of date string
+
+    Return:
+        A datetime object corrisponding to the string
+    """
     try:
         if flag:
             parsed_date = datetime.strptime(date, '%d-%b-%y')
@@ -183,9 +274,16 @@ def date_convert(date, flag):
     except: 
         raise("Error in date parsing: Format has changed.")
 
-       
-
 def make_request(url):
+    """
+    Make a request to 'url' and return the raw response.
+
+    Parameters:
+        * url (string): a URL to a newspaper aticle page
+    
+    Returns:
+        Raw response for url
+    """
     time.sleep(0.1)
     resp = requests.get(url)
     return resp
