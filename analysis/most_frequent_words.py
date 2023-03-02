fa0ec77a708f4b572952639248281b09f30f2d5b
MadeleineKRoberts <mkroberts@uchicago.edu>
2023-03-01 18:46:53 -0600

    Starting analysis

---
+++ analysis/most_frequent_words.py
@@ -0,0 +1,31 @@
+"""
+Project: CAPP 122 DataBased Project
+File name: most_frequent_words.py
+
+Finds and the most frequent words associated with a candidate or newspaper
+
+@Author: Madeleine Roberts
+@Date: Mar 1, 2023
+"""
+from collections import Counter
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+from utilities.data_retrieval import search_strings
+
+def most_frequent(id, db):
+    """
+
+    """
+
+    # TODO: pull the cleaned text from data base per the condition 
+
+    # iterate though text and count the number of occurances
+
+    # pull 100 most common words from the data 
+
+
+
+
+    
