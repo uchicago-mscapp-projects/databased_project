c3a1802be35fd9d80d9480653904ea6d76a109ee
MadeleineKRoberts <mkroberts@uchicago.edu>
2023-03-02 00:31:24 -0600

    most freq per cand per news

--- analysis/most_frequent_words.py
+++ analysis/most_frequent_words.py
@@ -36,13 +36,13 @@ def most_frequent():
 
     news_stopwords = ['said', 'also', 'would', 'city', 'former']
     
-    cand_word_freq = calc_most_frequent(df, "candidate_id", cand_stopwords, "Clean Strings")
-    
-    news_word_freq = calc_most_frequent(df, "Newspaper_id", news_stopwords, "Clean Text")
+    cand_word_freq = calc_most_frequent_single(df, "candidate_id", cand_stopwords, "Clean Strings")
+    news_word_freq = calc_most_frequent_single(df, "Newspaper_id", news_stopwords, "Clean Text")
+    cand_by_news_freq = calc_most_frequent_double(df, cand_stopwords)
+   
+    return cand_word_freq, news_word_freq, cand_by_news_freq
 
-    return cand_word_freq, news_word_freq
-
-def calc_most_frequent(df, token, additional_stop_words, text_to_inspect):
+def calc_most_frequent_single(df, token, additional_stop_words, text_to_inspect):
     unique_ids = df.loc[:,[token]].drop_duplicates()
     list_ids = unique_ids[token].values.tolist()
 
@@ -54,7 +54,7 @@ def calc_most_frequent(df, token, additional_stop_words, text_to_inspect):
         # Concatenate all pretaining text into one string
         full_text = ""
         for row in subset.index:
-            full_text += (df.iloc[row])["Clean Strings"]
+            full_text += (df.iloc[row])[text_to_inspect]
             full_text += " "
 
         words = Counter()
@@ -73,6 +73,53 @@ def calc_most_frequent(df, token, additional_stop_words, text_to_inspect):
         respective_word_dict[identifier] = freq_list
 
     return respective_word_dict
+
+def calc_most_frequent_double(df, additional_stop_words):
+    news_ids = df.loc[:,["Newspaper_id"]].drop_duplicates()
+    list_news_ids = news_ids["Newspaper_id"].values.tolist()
+
+    complete_dict = {}
+
+    for news_source in list_news_ids:
+
+        news_dict = {}
+       
+        cand_ids = df.loc[:,["candidate_id"]].drop_duplicates()
+        list_cand_ids = cand_ids["candidate_id"].values.tolist()
+
+
+        for candidate in list_cand_ids:
+           
+
+            subset = df.loc[df["Newspaper_id"] == news_source]
+            subset = subset.loc[df["candidate_id"] == candidate]
+
+            # Concatenate all pretaining text into one string
+            full_text = ""
+            for row in subset.index:
+                full_text += (df.iloc[row])["Clean Strings"]
+                full_text += " "
+
+            words = Counter()
+            stop_words = (stopwords.words('english'))
+            stop_words.extend(additional_stop_words)
+            
+            words.update(full_text.split())
+            most_common = words.most_common(50)
+
+            freq_list = []
+            for word_freq in most_common:
+                word, freq = word_freq
+                if word  not in  stop_words:
+                   
+                    freq_list.append(word_freq)
+            
+            news_dict[candidate] = freq_list
+        
+        complete_dict[news_source] = news_dict
+
+    return complete_dict
+        
 
 
 if __name__ == "__main__":
