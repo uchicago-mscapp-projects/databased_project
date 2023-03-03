b3b7c7fbd85e3a5677a3d03423306b86ad95c135
Abraham Justin Burton <abejburton@linux7.cs.uchicago.edu>
2023-03-03 12:50:46 -0600

    we got 2 plots now baby

--- data_viz/plots.py
+++ data_viz/plots.py
@@ -1,19 +1,13 @@
-from dash import html, Dash, dcc
+from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 
-
-cand_sentiment_filepath = "data/candidate_bs.json"
-news_sentiment_filepath = "data/news_bs.json"
-cand_news_sentiment_filepath = "data/cand_by_newspaper_bs.json"
-clean_articles_filepath = "data/clean_articles.json"
-
 # Read in the data
 cand_sentiment_df = pd.read_json('/home/abejburton/capp30122/databased_project/data/candidate_bs.json', orient='index')
 cand_sentiment_df['neg'] = cand_sentiment_df['neg'] * -1
-candidates = ['Kam Buckner','Chuy García',"Ja'Mal Green",'Brandon Johnson','Sophia King','Lori Lightfoot','Roderick Sawyer','Paul Vallas','Willie Wilson']
-cand_sentiment_df['candidates'] = candidates
+CANDIDATES = ['Kam Buckner','Chuy García',"Ja'Mal Green",'Brandon Johnson','Sophia King','Lori Lightfoot','Roderick Sawyer','Paul Vallas','Willie Wilson']
+cand_sentiment_df['candidates'] = CANDIDATES
 
 news_sentiment_df = pd.read_json('/home/abejburton/capp30122/databased_project/data/news_bs.json', orient='index')
 # cand_news_sentiment_df is a little weird w formatting hopefully it works for now
@@ -22,13 +16,34 @@ cand_news_sentiment_df = pd.read_json('/home/abejburton/capp30122/databased_proj
 # TODO get article info from maddie and also most common words.
 # clean_articles_df = pd.read_csv(pathlib.Path(__file__).parent.parent / clean_articles_filepath, usecols=['candidate_id', 'newspaper_id', 'url', 'date'], nrows = 50)
 
+count_cand_df = pd.read_json('/home/abejburton/capp30122/databased_project/analysis/data/count_cand.json', orient='index')
+count_cand_df.rename(columns={0:'mentions'}, inplace=True)
+MENTION_LABELS = ['Kam Buckner','Chuy García',"Ja'Mal Green",'Brandon Johnson','Sophia King','Roderick Sawyer','Paul Vallas','Willie Wilson','Lori Lightfoot','Total Articles','Total Unique Articles']
+count_cand_df['candidates'] = MENTION_LABELS
+count_cand_df.drop(['total_num_articles_scraped'], inplace=True)
 # Create the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-fig = px.bar(cand_sentiment_df, x='candidates', y=['pos','neg'], labels={'candidates':'Candidate', 'value': 'Sentiment'}, title='Sentiment Scores By Candidate')
-fig.layout.update(showlegend=False)
+mentions = px.bar(count_cand_df, x='candidates', y='mentions', labels={'candidates':'Candidate', 'mentions':'Number of Mentions'}, title='Number of Mentions By Candidate')
+sentiment = px.bar(cand_sentiment_df, x='candidates', y=['pos','neg'], labels={'candidates':'Candidate', 'value': 'Sentiment'}, title='Sentiment Scores By Candidate')
+sentiment.layout.update(showlegend=False)
 
-
+BODY = html.Div(children=[
+    html.Div([
+        html.H5(children='Mention'),
+        dcc.Graph(
+            id='mention_graph',
+            figure=mentions
+        ),
+    ]),
+    html.Div([
+        html.H5(children='Sentiment'),
+        dcc.Graph(
+            id='sentiment_graph',
+            figure=sentiment
+        ),
+    ]),
+])
 
 NAVBAR = dbc.Navbar(
     children=[
@@ -50,7 +65,7 @@ NAVBAR = dbc.Navbar(
     sticky="top",
 )
 
-app.layout = html.Div(children=[NAVBAR, dcc.Graph(id='example_graph', figure=fig)]) #, BODY
+app.layout = html.Div(children=[NAVBAR, BODY])
 
 if __name__ == "__main__":
     app.run_server(debug=False, port=8056)
