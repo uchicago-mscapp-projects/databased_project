fd0a77e1e7a369b3980d3c2ff2d35e3b7a1ca36a
Kathryn Jo Link-Oberstar <klinkoberstar@linux4.cs.uchicago.edu>
2023-03-03 22:44:44 -0600

    updates to name tokens for clean

--- utilities/clean.py
+++ utilities/clean.py
@@ -7,41 +7,51 @@ Associated files: None
 Primary Authors: Kathryn Link-Oberstar
 
 Clean JSON files of scraped articles.
+
 '''
-import csv
+
+'''
+Function Call
+papers = ["/data/crain.json", "/data/chicago_tribune.json", "/data/defender.json", 
+"/data/hph.json","/data/ln.json", "/data/triibe.json"]
+
+clean.export_clean(papers)
+'''
+#!python3 -m pip install nltk
+
+import nltk
 import sys
 import re
 import os
 import pandas as pd
 import json
-
+from nltk.corpus import stopwords
 from data_retrieval import search_strings
 
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
 
-STOP_WORDS = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 
-               'there', 'about', 'once', 'during', 'out', 'very', 'having', 
-               'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 
-               'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 
-               'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 
-               'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 
-               'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 
-               'himself', 'this', 'down', 'should', 'our', 'their', 'while', 
-               'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 
-               'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 
-               'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 
-               'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 
-               'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 
-               'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 
-               'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 
-               'it', 'how', 'further', 'was', 'here', 'than', 'chicago', 'mayor', 
-               'mayoral', 'we\'ve', 'we\'re', 'it\'s', 'he\s', 'she\'s']
+# nltk.download("stopwords")
+add_stop_words = ['mayor','representative','alderman', 'chicago', 'il'
+                                           'alderwoman', 'mayors', 'mayoral',
+                                           'congressman', 'amp', 'illinois', 
+                                           'said', 'would', 'city', 'also', 
+                                           'former','county', 'state', 
+                                           'commissioner', 'candidate', 
+                                           'candidates', 'raymond', 'lopez', 
+                                           'one', 'office', 'say', 'says', 'cook'
+                                           ,'de', 'get', 'que', 'la', 'el', 'en', 
+                                           'get', 'q', 'los', 'las', 'del', 'una', 
+                                           '000', 'un', 'gets','www','com', 'rep']
 
-STOP_WORDS_FORMAT = r'\b('+'|'.join(STOP_WORDS)+r')\b'
+stop_words = stopwords.words('english')
+stop_words.extend(add_stop_words)
+print(stop_words)
+stop_words_format = r'\b('+'|'.join(stop_words)+r')\b'
+print(stop_words_format)
 
-def clean(filepath, paper_id):
+def clean(filepath):
     '''
     This function takes the filepath of a JSON files containing a list of
     articles to clean. It returns an update list of articles, where three 
@@ -51,33 +61,35 @@ def clean(filepath, paper_id):
     with open(sys.path[-1] + filepath) as f:
         articles = json.load(f)
 
+    clean_name_tokens = pd.read_csv(sys.path[-1] + '/data/cleaning_name_tokens.csv')
+    cand_name_dict = (clean_name_tokens.groupby('candidate_id')['name_tokens']
+                        .apply(lambda x: [token.lower().strip() for token in set(x)])
+                        .to_dict())
+
     for article in articles:
         article["clean_title"] = clean_title(article)
         article["clean_text"] = clean_text(article)
-        article["clean_sentences"] = clean_sentences(article, article["clean_text"], paper_id)
+        article["clean_sentences"] = clean_sentences(article, article["clean_text"], cand_name_dict)
     return articles
 
 def clean_title(article):
     clean_title = article['title'].strip().lower()
-    remove_words_title = re.sub(STOP_WORDS_FORMAT,' ', clean_title)
+    remove_words_title = re.sub(stop_words_format,' ', clean_title)
     remove_punct = re.sub(r'[^\w\s]', '', remove_words_title)
     clean_title = re.sub(r'\s+',' ', remove_punct)
     return clean_title
 
 def clean_text(article):
     clean_text = article['text'].lower()
-    remove_words = re.sub(STOP_WORDS_FORMAT,' ', clean_text)
-    remove_ald = re.sub(r'(ald\.|rep\.)',' ', remove_words)
-    remove_punct = re.sub(r'[^\w\s.!?]+',' ', remove_ald) # remove tabs
+    remove_words = re.sub(stop_words_format,' ', clean_text)
+    remove_ald = re.sub(r'(ald\.|rep\.|vs\.)',' ', remove_words)
+    remove_punct = re.sub(r'[^\w\s.!?]+',' ', remove_ald)
     replace_end = re.sub(r'[\n?!]', '.', remove_punct)
     clean_text = re.sub(r'\s+',' ', replace_end)
     return clean_text
 
-def clean_sentences(article, clean_text, paper_id):
-    newpaper_db = search_strings(newspaper_id = paper_id)
-    cand_name_dict = (newpaper_db.groupby('candidate_id')['name_tokens']
-                        .apply(lambda x: [token.lower().strip() for token in set(x)])
-                        .to_dict())
+def clean_sentences(article, clean_text, cand_name_dict):
+    
     final_sent = []
     for sent in clean_text.split("."):
         sent_strip = sent.strip()
@@ -89,46 +101,25 @@ def clean_sentences(article, clean_text, paper_id):
                         break
     return ' '.join(final_sent)
 
-papers = [("/data/crain.json", 'news_cc'), ("/data/chicago_tribune.json", 'news_ct'), 
-          ("/data/defender.json", 'news_cd'), ("/data/hph.json", 'news_hp'), 
-          ("/data/ln.json", 'news_ln'), ("/data/triibe.json", 'news_tt')]
-
-# Export Clean Articles
 def export_clean(papers):
     '''
     Takes list of tuples where the tuple is the filepath to the paper's JSON 
-    file of articles and the newspaper id
-    For example: [("/data/crain.json", 'news_cc'), 
-    ("/data/chicago_tribune.json", 'news_ct')] would return the cleaned articles
-    for Crain Business journal and the Chicago Tribune.
+    file of articles and the newspaper id.
     '''
     all_articles = []
     for paper in papers:
-        filename, paper_id = paper
-        all_articles += clean(filename, paper_id)
+        all_articles += clean(paper)
     
     df = pd.DataFrame(all_articles)
     dedupe_df = df.drop_duplicates(subset = ['candidate_id', 'url'], 
                                 keep = 'last').reset_index(drop = True)
     print(dedupe_df)
-    print("Writing CSV")
-    filepath = sys.path[-1] + '/data/clean_articles.csv'
-    dedupe_df.to_csv(filepath, index=False)
-
-    """
-    print("Writing CSV")
-    filepath = sys.path[-1] + '/data/clean_articles.csv'
-    keys = ["candidate_id", "name_tokens", "announcement_date", "newspaper_id", 
-            "url", "date", "title", "text", "clean_title", "clean_text", 
-            "clean_sentences"]
-    with open(filepath, 'w', newline='') as f:
-        writer = csv.DictWriter(f, fieldnames=keys)
-        writer.writeheader()
-        for article in all_articles:
-            writer.writerow(article)
-    """
-
-    # For Debugging
-    print("Writing json")
+    '''
+    print("Writing clean_articles json")
     filepath = sys.path[-1] + '/data/clean_articles.json'
-    df.to_json(filepath, orient='records')
+    dedupe_df.to_json(filepath, orient='records')
+    '''
+    print("Writing clean_articles_ abridged.json")
+    filepath_abr = sys.path[-1] + '/data/clean_articles_abr.json'
+    dedupe_df_drop = dedupe_df.drop(['title','text','clean_text'], axis = 1)
+    dedupe_df_drop.to_json(filepath_abr, orient= 'records')
