b7fead1a4c8951c537b942b1ce24a3daf3b57ce0
Kathryn Jo Link-Oberstar <klinkoberstar@linux4.cs.uchicago.edu>
2023-03-04 17:14:10 -0600

    update docstrings and set up for main

--- scrapers/tribune_crain_process.py
+++ scrapers/tribune_crain_process.py
@@ -2,12 +2,11 @@
 Project: Analyzing News Coverage of Chicago's 2023 Mayoral Election
 Team: dataBASED
 
-File name: process_articles.py
-Associated files: select_articles_ct_cc.py
+File name: tribune_crain_process.py
+Associated files: tribune_crain_select.py
 Primary Author: Kathryn Link-Oberstar
-Addiditional Authors: None
 
-Description: Decompress Proquest Data Files, and process articles to create a 
+Description: Decompress Proquest Data Files, and process articles to create a
 list of dictionaries, one for each article.
 '''
 import sys
@@ -17,26 +16,26 @@ import json
 import pandas as pd
 
 STRINGS_TO_REMOVE = ["<meta name='ValidationSchema' content='http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd'/>",
-"</i>", "</p>", "</body>", "</html>", "<head>", "<title>", "</title>", 
+"</i>", "</p>", "</body>", "</html>", "<head>", "<title>", "</title>",
 "</head>", "<body>", "<p>", "<html>", "<b>", "<i>", "</b>"]
 
 def unpack_file(tar, parquet):
     '''
-    Extract compressed files from tarball and parquet and return as a pandas 
+    Extract compressed files from tarball and parquet and return as a pandas
     dataframe.
-    
+
     Inputs:
         tar (string): filepath to tar file
         parquet (string): filepath to parquet file
     Output:
-        df_jsons (pandas dataframe): pandas dataframe of JSON files, where 
+        df_jsons (pandas dataframe): pandas dataframe of JSON files, where
             each file is an article
     '''
-    tz_file = tarfile.open(tar)
-    parquet_file = tz_file.extractall(sys.path[-1] + 'data/proquest_files')
-    parquet_file = tz_file.extractfile(parquet)
-    df_jsons = pd.read_parquet(parquet_file)
-    tz_file.close()
+    with tarfile.open(tar) as tz_file:
+        parquet_file = tz_file.extractall(sys.path[-1] + 'data/proquest_files')
+        parquet_file = tz_file.extractfile(parquet)
+        df_jsons = pd.read_parquet(parquet_file)
+        tz_file.close()
     return df_jsons
 
 def convert_to_dict(tar, parquet, newspaper_id, url_counter = 0):
@@ -46,21 +45,23 @@ def convert_to_dict(tar, parquet, newspaper_id, url_counter = 0):
         * 'candidate_id'
         * 'name_tokens'
         * 'announcement_date'
-        * 'Newspaper_id'
-        * 'Url'
-        * 'Title'
-        * 'Text' 
+        * 'newspaper_id'
+        * 'url'
+        * 'title'
+        * 'text'
         * 'date'
-    The values for the last 5 keys will be assigned in this function with data 
+    The values for the last 5 keys will be assigned in this function with data
     from the article JSON. The first 3 will be initialized to None, and will
     have values assigned later when the articles are paired with a candidate.
-    
+    Since URLs do not exist for these articles, each article will be assigned a
+    unique number in the URL field.
+
     Inputs:
         tar (string): filepath to tar file
         parquet (string): filepath to parquet file
         newspaper_id (string): Unique ID for the newspaper being analyzed
     Output:
-        df_jsons (pandas dataframe): pandas dataframe of JSON files, where 
+        df_jsons (pandas dataframe): pandas dataframe of JSON files, where
             each file is an article
     '''
     df_jsons = unpack_file(tar, parquet)
@@ -73,11 +74,9 @@ def convert_to_dict(tar, parquet, newspaper_id, url_counter = 0):
         url = url_counter
         title = row_json["RECORD"]['Obj']['TitleAtt']['Title']
         pub_date = row_json["RECORD"]['Obj']['NumericDate']
-        result_dict = {'candidate_id': None, 'name_tokens': None, 
-                       'announcement_date': None, 'newspaper_id': newspaper_id, 
-                       'url': url, 'title': title, 'text': text_clean, 
+        result_dict = {'candidate_id': None, 'name_tokens': None,
+                       'announcement_date': None, 'newspaper_id': newspaper_id,
+                       'url': url, 'title': title, 'text': text_clean,
                        'date': pub_date}
         all_results.append(result_dict)
     return all_results
-
-
